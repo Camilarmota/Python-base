a23c53de27f76a363e7d447e1ae31d43d514f2fb
Camila <140784690+Camilarmota@users.noreply.github.com>
2025-08-29 21:20:52 +0000

    nova versão do arquivo tabuada.py

--- tabuada.py
+++ tabuada.py
@@ -13,7 +13,7 @@ Tabuada do 2
 _________________________________
 
  """
-__version__ = "1.0.0"
+__version__ = "0.1.1"
 __author__ = "Camila Mota"
 
 # numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -22,8 +22,16 @@ numeros = list(range(1, 11))
 # Interable (percorríveis)
 
 # Para cada número em numeros
-for numero in numeros:
+"""for numero in numeros:
     print("tabuada do numero:", numero)
     for outro_numero in numeros:
         print(numero * outro_numero)
-    print("-" * 30)
+    print("-" * 30)"""
+
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print("#" * 18)))
